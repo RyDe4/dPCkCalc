7b272abbebcc154008fa0fdbc0bd058526414a65
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-09 17:16:44 -0400

    Renaming package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = 'DispersalCalc',
+    name = 'dPCkCalc',
     version = '0.0.x',
     description = '',
-    py_modules = ["Pc_Calc"],
-    package_dir = {'': 'DispersalCalc'}
+    py_modules = ["PcCalc", "Visualization"],
+    package_dir = {'': 'dPCkCalc'}
 )
