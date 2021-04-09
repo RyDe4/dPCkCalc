31316b6cb0f999b95cbcb102812ec666c5c74b33
Ryan Denniston <73243752+RyDe4@users.noreply.github.com>
2021-04-09 12:18:29 -0400

    Delete venv/Scripts directory

--- venv/Scripts/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = 'DispersalCalc',
-    version = '0.0.x',
-    description = '',
-    py_modules = ["Pc_Calc"],
-    package_dir = {'': 'DispersalCalc'}
-)
