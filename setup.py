ed9a354aa0ac17957b1ab5cb77796708d69e2ad3
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-11 00:14:47 -0400

    making dependencies install automatically, allowing for import of whole package at once instead of individual modules

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version = '0.0.x',
     description = '',
     py_modules = ["PcCalc", "Visualization"],
+    packages = ["dPCkCalc"],
+    install_requires = ["numpy", "geopandas", "igraph", "seaborn"],
     package_dir = {'': 'dPCkCalc'}
 )
