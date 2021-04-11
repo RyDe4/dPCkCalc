40403a8e804a26e4dce9c542db16526273187726
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-11 00:28:38 -0400

    adding descartes to dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version = '0.0.x',
     description = '',
     py_modules = ["PcCalc", "Visualization"],
-    install_requires = ["numpy", "shapely", "geopandas", "igraph", "seaborn"],
+    install_requires = ["numpy", "descartes", "shapely", "geopandas", "igraph", "seaborn"],
     package_dir = {'': 'dPCkCalc'}
 )
