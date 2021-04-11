fb36dbf319c4135efe0170949ab25be65771d9a6
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-11 00:17:06 -0400

    adding updates

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version = '0.0.x',
     description = '',
     py_modules = ["PcCalc", "Visualization"],
-    packages = ["dPCkCalc"],
     install_requires = ["numpy", "geopandas", "igraph", "seaborn"],
     package_dir = {'': 'dPCkCalc'}
 )
