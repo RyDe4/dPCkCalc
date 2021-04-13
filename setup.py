812ac290cb642d6c57fe2b81cb2be34429a8a53d
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-12 23:41:48 -0400

    adding matplotlib to dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version = '0.0.x',
     description = '',
     py_modules = ["PcCalc", "Visualization"],
-    install_requires = ["numpy", "descartes", "shapely", "geopandas", "igraph", "seaborn"],
+    install_requires = ["numpy", "igraph", "matplotlib", "descartes", "shapely", "geopandas", "seaborn"],
     package_dir = {'': 'dPCkCalc'}
 )
