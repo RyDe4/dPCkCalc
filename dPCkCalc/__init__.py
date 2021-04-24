b91e48d618cbb3bc8964e578c30947288dc6593a
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-04-23 21:13:57 -0400

    improved documentation for visualization functions and removed imports for undocumented functions

--- dPCkCalc/__init__.py
+++ dPCkCalc/__init__.py
@@ -9,9 +9,7 @@ from dPCkCalc.PcCalc import load_csv_data
 from dPCkCalc.PcCalc import calc_dpc_all
 from dPCkCalc.PcCalc import load_matrix
 from dPCkCalc.Visualization import load_pc_data
-from dPCkCalc.Visualization import scatter_edge_dist
 from dPCkCalc.Visualization import map_visualize
 from dPCkCalc.Visualization import graph_map
 from dPCkCalc.Visualization import pie_composition
-from dPCkCalc.Visualization import get_in_bounds
 from dPCkCalc.Visualization import make_igraph_graph
